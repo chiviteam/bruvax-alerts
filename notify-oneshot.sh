62a1dcf75912bda1e460bc5e89ccffd6e892c591
gerg <gerg@home.be>
2021-05-28 17:26:10 +0200

    Oneshot: 2021-05-28T15:26:10 UTC

--- notify-oneshot.sh
+++ notify-oneshot.sh
@@ -5,6 +5,7 @@ do
     npm run oneshot
     git add -A
     timestamp=$(date -u)
+    echo "$timestamp"
     git commit -m "Oneshot: ${timestamp}" || continue
     echo "something changed"
     kdeconnect-cli -d $(kdeconnect-cli -a --id-only)  --ping-msg "something changed"
