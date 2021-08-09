5b2fe3be22955d84bca98ab56490ec4e5fd27c04
gerg <gerg@home.be>
2021-08-09 17:04:59 +0200

    removing some checks

--- index.js
+++ index.js
@@ -14,47 +14,7 @@ const lib = require("./lib");
             scrapeUrl: "https://www.splashbrussel.be/inschrijving",
             selector: "main",
             id: "splash"
-        },
-        {
-            scrapeUrl: "https://www.eendjesschaarbeek.be/practice_areas",
-            selector: 'main',
-            id: "eendjes"
-        },
-        {
-            scrapeUrl: "http://fermedejette.be/nl/de-stages/",
-            selector: 'main',
-            id: "ferme-nl"
-        },
-        {
-            scrapeUrl: "https://www.guppies.be/",
-            selector: '.template',
-            id: "guppies"
-        }/*,
-        {
-            scrapeUrl: "https://www.n22.brussels/activiteiten-zoeken?f%5B0%5D=field_activiteit_doelgroep%3A1347&f%5B1%5D=gc_gemeente_gc%3AGC%20Essegem%20%23%23%23%20Jette",
-            selector: '#block-system-main',
-            id: "essegem-kleuters"
-        },
-        {
-            scrapeUrl: "https://www.n22.brussels/activiteiten-zoeken?f%5B0%5D=field_activiteit_doelgroep%3A1347&f%5B1%5D=gc_gemeente_gc%3AGC%20De%20Zeyp%20%23%23%23%20Ganshoren",
-            selector: '#block-system-main',
-            id: "dezeyp-kleuters"
-        },
-        {
-            scrapeUrl: "https://www.n22.brussels/activiteiten-zoeken?f%5B0%5D=field_activiteit_doelgroep%3A1347&f%5B1%5D=gc_gemeente_gc%3AGC%20Nekkersdal%20%23%23%23%20Laken",
-            selector: '#block-system-main',
-            id: "nekkersdal-kleuters"
-        },
-        {
-            scrapeUrl: "https://www.n22.brussels/activiteiten-zoeken?f%5B0%5D=field_activiteit_doelgroep%3A1347&f%5B1%5D=gc_gemeente_gc%3AGC%20De%20Platoo%20%23%23%23%20Koekelberg",
-            selector: '#block-system-main',
-            id: "deplatoo-kleuters"
-        },
-        {
-            scrapeUrl: "https://www.covidsafe.be/",
-            selector: 'body',
-            id: "covidsafe"
-        }*/
+        }
     ]
 
     lib.grabAll(checks);
