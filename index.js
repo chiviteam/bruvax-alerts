119a99af4e507b0d593633774a1f2b281ccf1443
gerg <gerg@home.be>
2021-09-13 09:08:32 +0200

    remove unused diplo

--- index.js
+++ index.js
@@ -26,40 +26,11 @@ const lib = require("./lib");
             id: "diplo-montenegro"
         },
         {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/macedoine_du_nord",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-macedonia"
-        },
-        {
             scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/egypte",
             selector: "#block-system-main .field-name-body .field-item table",
             id: "diplo-egypt"
-        },
-        {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/islande",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-iceland"
-        },
-        {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/taiwan",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-taiwan"
-        },
-        {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/japon",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-japan"
-        },
-        {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/costa_rica",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-costa-rica"
-        },
-        {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/jordanie",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-jordania"
         }
+         
     ]
 
     lib.grabAll(checks);
