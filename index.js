dcff11f6de56702ca92087bcd54f5c24f3566719
greg endeavour vostro <greg-endeavour@vostro.be>
2021-11-23 19:50:09 +0100

    disabling stuff

--- index.js
+++ index.js
@@ -11,21 +11,6 @@ const lib = require("./lib");
             id: "circus"
         },
         {
-            scrapeUrl: "https://www.splashbrussel.be/inschrijving",
-            selector: "main",
-            id: "splash"
-        },
-        {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/albanie",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-albanie"
-        },
-        {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/montenegro",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-montenegro"
-        },
-        {
             scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/egypte",
             selector: "#block-system-main .field-name-body .field-item table",
             id: "diplo-egypt"
