45abf9e1071ff5507abbd06b86e637ae2b2b9a70
Gregory Vandenbroucke <gvdenbro@users.noreply.github.com>
2022-06-21 16:35:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,41 +6,6 @@ const lib = require("./lib");
 
     const checks = [
         {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/maldives",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-maldives"
-        },
-        {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/georgie",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-georgie"
-        },
-        {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/thailande",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-thailande"
-        },
-        {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/egypte",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-egypt"
-        },
-        {
-            scrapeUrl: "https://diplomatie.belgium.be/fr/Services/voyager_a_letranger/conseils_par_destination/roumanie",
-            selector: "#block-system-main .field-name-body .field-item table",
-            id: "diplo-roumanie"
-        },/*
-        {
-            scrapeUrl: "https://www.nekkersdal.be/activiteiten?f%5B0%5D=activity_target_group%3A165&f%5B1%5D=activity_target_group%3A576&items_per_page=All",
-            selector: "#block-system-main-block",
-            id: "gc-nekkersdal"
-        },
-        {
-            scrapeUrl: "https://www.essegem.be/activiteiten?items_per_page=All&f%5B0%5D=activity_target_group%3A165&f%5B1%5D=activity_target_group%3A576",
-            selector: "#block-system-main-block",
-            id: "gc-essegem"
-        },*/
-        {
             scrapeUrl: "https://terresneuves.be/enfants/",
             selector: '#content',
             id: "terres-neuves"
