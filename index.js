ee10b60285fbd6df5140bbe0bad4e92c17fb79a0
Gregory Vandenbroucke <gvdenbro@users.noreply.github.com>
2022-09-10 20:43:01 +0200

    checking for hilary hahn events at bozar

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ const lib = require("./lib");
             scrapeUrl: "https://terresneuves.be/enfants/",
             selector: '#content',
             id: "terres-neuves"
-        },
+        }/*,
         {
             scrapeUrl: "https://www.autoscout24.be/fr/lst/dacia/sandero?fregfrom=2015&sort=age&desc=1&cy=B&atype=C&ustate=N%2CU&powertype=kw&priceto=6000",
             selector: 'main > article',
             multiple: true,
             id: "autoscout-sandero"
+        }*/,
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=hahn",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-hilary-hahn"
         }
     ]
 
