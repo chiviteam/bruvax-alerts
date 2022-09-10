a6f8ae7fa80d9c463443666780784ee46e0371c3
gerg <gerg@ec.eu>
2022-09-10 21:17:54 +0200

    adding a couple of bozar artists

--- index.js
+++ index.js
@@ -21,6 +21,84 @@ const lib = require("./lib");
             selector: '.search-page__results-list > article:not(.hidden)',
             multiple: true,
             id: "bozar-hilary-hahn"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=chen",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-chen"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=jansen",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-jansen"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=argerich",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-argerich"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=belcea",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-belcea"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=balanescu",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-balanescu"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=kennedy",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-kennedy"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=kronos",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-kronos"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=jumi",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-jumi"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=kang",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-kang"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=chang",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-chang"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=joshua",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-joshua"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=gabetta",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-gabetta"
+        },
+        {
+            scrapeUrl: "https://www.bozar.be/fr/search?contentType=event&searchQuery=savall",
+            selector: '.search-page__results-list > article:not(.hidden)',
+            multiple: true,
+            id: "bozar-savall"
         }
     ]
 
